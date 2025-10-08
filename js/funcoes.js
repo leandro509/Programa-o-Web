5653d4204b8ba69cca503946c3d7c906ae72cb95
Leandro Gabriel de Oliveira Nascimento <lgonascimento@furb.br>
2025-10-07 21:28:16 -0300

    funcoes javascript

--- js/funcoes.js
+++ js/funcoes.js
@@ -9,3 +9,64 @@ function ativaClasse1() {
     var vElemento = document.getElementById('mensagem');
     vElemento.classList.add('classe1');
 }
+
+function ativaClasse2() {
+    var vElemento = document.getElementById('mensagem');
+    vElemento.classList.add('classe2');
+}
+
+function removeClasse1() {
+    var vElemento = document.getElementById('mensagem');
+    vElemento.classList.remove('classe1');
+}
+
+function toggleClasse2() {
+    var vElemento = document.getElementById('mensagem');
+    vElemento.classList.toggle('classe2');
+}
+
+function adicionaEvento() {
+    var vElemento = document.getElementById('fazNada');
+    vElemento.addEventListener('click',
+        () => { alert('Agora faz alguma coisa') }
+    );
+}
+
+function adicionarTarefa() {
+    var vInput = document.getElementById('tarefa');
+    var vLista = document.getElementById('listaTarefas');
+    var vItemLista = document.createElement('li');
+    vItemLista.innerHTML = vInput.value;
+    vLista.appendChild(vItemLista);
+
+    vInput.value = '';
+    vInput.focus();
+}
+
+function adicionarNoInicio() {
+    var vInput = document.getElementById('tarefa');
+    var vLista = document.getElementById('listaTarefas');
+    var vItemLista = document.createElement('li');
+
+    vItemLista.innerHTML = vInput.value;
+    vLista.insertBefore(vItemLista, vLista.firstChild);
+    vInput.value = '';
+    vInput.focus();
+}
+    
+function removerItem() {
+    var vLista = document.getElementById('listaTarefas');
+    vLista.removeChild(vLista.lastChild);
+}
+
+function armazenaLS() {
+    localStorage.setItem("infoLS", "Informação do Local Storage");
+}
+
+function armazenaSS() {
+    sessionStorage.setItem("infoSS", "Informação do Session Storage");
+}
+
+function infoLS() {
+    document.getElementById('mensagem').innerHTML = localStorage.getItem('infoLS');
+}
