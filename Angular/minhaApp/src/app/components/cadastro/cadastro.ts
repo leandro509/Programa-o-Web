c406a037e227dd84c02f10efb13bff71a8719bde
Leandro Gabriel de Oliveira Nascimento <lgonascimento@furb.br>
2025-11-04 20:57:20 -0300

    att uso framework

--- Angular/minhaApp/src/app/components/cadastro/cadastro.ts
+++ Angular/minhaApp/src/app/components/cadastro/cadastro.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { Pessoa } from '../../interfaces/pessoa';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
 export class Cadastro {
+  //vPessoa não é uma variavel.
+  vPessoa: Pessoa = {
+    idPessoa: 1,
+    nmPessoa: 'Marcos',
+    nmCidade: 'Blumenau',
+    vlSalario: 5000
+  };
+  vPcAumento: number = 0;
 
+  vetPessoas: Pessoa[] = [
+    {
+      idPessoa: 2,
+      nmPessoa: 'Cardoso',
+      nmCidade: 'Blumenau',
+      vlSalario: 5000
+    },
+    {
+      idPessoa: 3,
+      nmPessoa: 'Aurelio',
+      nmCidade: 'Ilhota',
+      vlSalario: 10000
+    },
+    {
+      idPessoa: 4,
+      nmPessoa: 'Alexander',
+      nmCidade: 'Jaragua',
+      vlSalario: 8000
+    }
+  ];
+
+  calcularAumento() {
+    this.vPessoa.vlSalario *= (this.vPcAumento / 100) + 1;
+  }
+  
+  editaPessoa (pPessoa: Pessoa) {
+    this.vPessoa = pPessoa;
+  }
 }
---
+++ Angular/minhaApp/src/app/interfaces/pessoa.ts
@@ -0,0 +1,6 @@
+export interface Pessoa {
+    idPessoa: number,
+    nmPessoa: string,
+    nmCidade: string,
+    vlSalario: number
+}
